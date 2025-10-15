a7f656f5843617bca24ec57c26824cdac3d35337
Pranto Biswas <prantobd320@gmail.com>
2025-10-15 23:45:02 +0600

    update : google adsense

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <meta name="google-adsense-account" content="ca-pub-1076115507843658">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
