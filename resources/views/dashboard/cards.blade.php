a60a3a0462bc81dd55c72cdaaf38a9d9c3877ffa
Pranto Biswas <prantobd320@gmail.com>
2025-12-15 00:58:43 +0600

    update : Processing icon

--- resources/views/dashboard/cards.blade.php
+++ resources/views/dashboard/cards.blade.php
@@ -65,7 +65,7 @@
                 <!-- Card Details -->
                 <div class="flex-1 grid grid-cols-2 md:grid-cols-6 gap-6">
                     <div>
-                        <div class="text-sm text-gray-500 mb-1">Card Type:</div>
+                        <div class="text-sm text-gray-500 mb-1">Card Organization</div>
                         <div class="text-sm font-medium text-gray-800">{{ $card->organization }}</div>
                     </div>
                     <div>
@@ -94,8 +94,13 @@
                 <div class="flex items-center space-x-4">
                     @if($card->state == 4)
                     <div
-                        class="bg-yellow-600 text-white hover:text-blue-800 hover:cursor-not-allowed text-sm font-medium transition-colors border border-yellow-600 px-3 py-1 rounded-lg hover:bg-blue-50 ">
-                        Pending</div>
+                        class="bg-yellow-600 text-white hover:cursor-not-allowed text-sm font-medium transition-colors border border-yellow-600 px-3 py-1 rounded-lg flex items-center space-x-2">
+                        <svg class="animate-spin h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        <span>Processing</span>
+                    </div>
                     @else
                     <a href="{{ route('view_card', $card->id) }}"
                         class="text-blue-600 hover:text-blue-800 text-sm font-medium transition-colors border border-blue-600 px-3 py-1 rounded-lg hover:bg-blue-50">
