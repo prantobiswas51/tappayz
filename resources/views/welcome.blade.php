19516b47b7cea9879a6df5ae4f7977aa41860979
Pranto Biswas <prantobd320@gmail.com>
2025-12-16 00:16:43 +0600

    update : video update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -253,7 +253,7 @@
                 <div class="relative bg-gray-900 rounded-2xl overflow-hidden mb-8">
                     <div class="aspect-video">
                         <iframe width="100%" height="100%"
-                            src="https://www.youtube.com/embed/0F-Qh0oRJDQ?si=916c3YfLncv04xzU&amp;start=41"
+                            src="https://www.youtube.com/embed/ZEcguQjkqnI?si=jEzxMfnAh_5uxmdn&amp;start=2"
                             title="YouTube video player" frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
